165edef13374b80eea2d956dac964894e39e441e
adarsh-scanta <91716659+adarsh-scanta@users.noreply.github.com>
2022-10-27 02:16:42 +0530

    new build

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ export default class App extends PureComponent {
     return (<>
       <div>
         <button
-          className="block px-10 p-2 shadow-md rounded-md"
+          className="block px-10 p-2 shadow-md rounded-md mx-auto"
           type="button" data-modal-toggle="defaultModal">
           <div>
             <img src="https://webenginefe.deveast.scanta.io/trupulse-logo.png" alt="logo" width="80px" />
-            <p className="text-sm">for {this.props.companyName}</p>
+            <p className="text-sm text-gray-400">For {this.props.companyName}</p>
           </div>
         </button>
 
@@ -27,7 +27,7 @@ export default class App extends PureComponent {
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
               <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                <img src="https://webenginefe.deveast.scanta.io/trupulse-logo.png" alt="logo" width="100px" />
+                  <img src="https://webenginefe.deveast.scanta.io/trupulse-logo.png" alt="logo" width="100px" />
                 </h3>
                 <button type="button"
                   className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -41,16 +41,64 @@ export default class App extends PureComponent {
                   <span className="sr-only">Close modal</span>
                 </button>
               </div>
-              <div className="p-6 space-y-6">
+              <div className="px-6 pt-4 pb-2 space-y-2">
                 <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                 <span className="font-semibold">
-                  Morale Score
-                  </span> 
+                  <span className="font-extrabold">
+                    Morale Score
+                  </span>
                 </p>
                 <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
                   <span className="font-semibold">High: </span>{"67%"}
                 </p>
               </div>
+              <div className="px-6 pt-4 pb-4 space-y-2">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-extrabold">
+                    Trending Topics
+                  </span>
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  appreciation,Compensation, Harrasment, gender equality, promotion, technology, management
+                </p>
+              </div>
+              <div className="px-6 pt-4 pb-4 space-y-2">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-extrabold">
+                    Sentiments
+                  </span>
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="mr-4">
+                    <span className="font-semibold">Positive: </span>{"72%"}
+                  </span>
+                  <span className="ml-4">
+                    <span className="font-semibold">Negative: </span>{"28%"}
+                  </span>
+                </p>
+              </div>
+              <div className="px-6 pt-4 pb-2 space-y-2">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-extrabold">
+                    Emotion
+                  </span>
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Joy: </span>{"42%"}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Anger: </span>{"12%"}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Sad: </span>{"0%"}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Fear: </span>{"0%"}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Surprise: </span>{"0%"}
+                </p>
+              </div>
+
               {/* <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
                 <button data-modal-toggle="defaultModal" type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
